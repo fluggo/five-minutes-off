12e512690cc79a7ad8db7aa139734213e169b817
Brian Crowell <brian@fluggo.com>
2018-06-11 18:47:01 -0500

    Basic server with most (but not all) tests

---
+++ server/funcs.spec.ts
@@ -0,0 +1,206 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as async from 'async';
+import * as funcs from './funcs';
+
+function cleanUp() {
+  return Promise.all([
+    funcs.deleteWeeksTable().catch(err => {
+      if(err.code === 'ResourceNotFoundException')
+        return Promise.resolve();
+
+      return Promise.reject(err);
+    }),
+    funcs.deleteAccountsTable().catch(err => {
+      if(err.code === 'ResourceNotFoundException')
+        return Promise.resolve();
+
+      return Promise.reject(err);
+    }),
+  ])
+}
+
+describe('Accounts', function() {
+  const ACCOUNT_ID = 'BLAHBLONK';
+
+  describe('createAccount', function() {
+    beforeEach(async function() {
+      await cleanUp();
+      await funcs.createAccountsTable();
+    });
+
+    afterEach(function() {
+      return cleanUp();
+    });
+
+    it('creates an account', function() {
+      return funcs.createAccount(ACCOUNT_ID);
+    });
+
+    it('fails if the account already exists', async function() {
+      await funcs.createAccount(ACCOUNT_ID);
+
+      return funcs.createAccount(ACCOUNT_ID).then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('account-exists');
+        }
+      );
+    });
+  });
+
+  describe('setWeek', function() {
+    beforeEach(async function() {
+      await cleanUp();
+      await Promise.all([
+        funcs.createAccountsTable(),
+        funcs.createWeeksTable(),
+      ]);
+      await funcs.createAccount(ACCOUNT_ID);
+    });
+
+    afterEach(function() {
+      return cleanUp();
+    });
+
+    const WEEK1 = '2018-W05';
+
+    it('sets up a week', async function() {
+      const week = await funcs.setWeek(ACCOUNT_ID, WEEK1, 10);
+
+      expect(week.weekID).to.equal(WEEK1);
+      expect(week.changes).to.have.lengthOf(0);
+      expect(week.minutesGranted).to.equal(10);
+      expect(week.updateID).to.exist;
+      expect(week.accountID).to.equal(ACCOUNT_ID);
+    });
+
+    it("allows zero time", async function() {
+      const week = await funcs.setWeek(ACCOUNT_ID, WEEK1, 0);
+      expect(week.minutesGranted).to.equal(0);
+    });
+
+    it("changes the updateID", async function() {
+      const week1 = await funcs.setWeek(ACCOUNT_ID, WEEK1, 100);
+      const week2 = await funcs.setWeek(ACCOUNT_ID, WEEK1, 10);
+
+      expect(week1.updateID).to.not.equal(week2.updateID);
+    });
+
+    it("fails if a negative time is provided", function() {
+      return funcs.setWeek(ACCOUNT_ID, WEEK1, -10).then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('insufficient-time');
+        }
+      );
+    });
+
+    it("fails if the weekID is invalid", function() {
+      return funcs.setWeek(ACCOUNT_ID, '2018-02-15', 0).then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('invalid-week');
+        }
+      );
+    });
+
+    it("fails if the accountID is invalid", function() {
+      return funcs.setWeek('skimpopple', WEEK1, 0).then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('invalid-account');
+        }
+      );
+    });
+
+    it("fails if time would go negative", async function() {
+      await funcs.setWeek(ACCOUNT_ID, WEEK1, 100);
+      await funcs.addTime(ACCOUNT_ID, WEEK1, -10, 'Not listening');
+      await funcs.setWeek(ACCOUNT_ID, WEEK1, 10);
+
+      await funcs.setWeek(ACCOUNT_ID, WEEK1, 0).then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('insufficient-time');
+        }
+      );
+    });
+  });
+
+  describe('addTime', function() {
+    beforeEach(async function() {
+      await cleanUp();
+      await Promise.all([
+        funcs.createAccountsTable(),
+        funcs.createWeeksTable(),
+      ]);
+      await funcs.createAccount(ACCOUNT_ID);
+    });
+
+    afterEach(function() {
+      return cleanUp();
+    });
+
+    const WEEK1 = '2018-W05';
+
+    it('basic usage', async function() {
+      await funcs.setWeek(ACCOUNT_ID, WEEK1, 300);
+      let record1 = await funcs.addTime(ACCOUNT_ID, WEEK1, -5, 'Not listening');
+
+      expect(record1.weekID).to.equal(WEEK1);
+      expect(record1.changes).to.have.lengthOf(1);
+      expect(record1.changes[0].minutesAdded).to.equal(-5);
+      expect(record1.changes[0].reason).to.equal('Not listening');
+      expect(record1.changes[0].time).to.match(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ$/);
+      expect(record1.minutesGranted).to.equal(300);
+      expect(record1.updateID).to.exist;
+      expect(record1.accountID).to.equal(ACCOUNT_ID);
+
+      record1 = await funcs.getWeek(ACCOUNT_ID, WEEK1);
+
+      expect(record1.weekID).to.equal(WEEK1);
+      expect(record1.changes).to.have.lengthOf(1);
+      expect(record1.changes[0].minutesAdded).to.equal(-5);
+      expect(record1.changes[0].reason).to.equal('Not listening');
+      expect(record1.changes[0].time).to.match(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ$/);
+      expect(record1.minutesGranted).to.equal(300);
+      expect(record1.updateID).to.exist;
+      expect(record1.accountID).to.equal(ACCOUNT_ID);
+
+      let record2 = await funcs.addTime(ACCOUNT_ID, WEEK1, -295, 'REALLY not listening');
+
+      expect(record2.weekID).to.equal(WEEK1);
+      expect(record2.changes).to.have.lengthOf(2);
+      expect(record2.changes[0].minutesAdded).to.equal(-5);
+      expect(record2.changes[0].reason).to.equal('Not listening');
+      expect(record2.changes[0].time).to.match(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ$/);
+      expect(record2.changes[1].minutesAdded).to.equal(-295);
+      expect(record2.changes[1].reason).to.equal('REALLY not listening');
+      expect(record2.changes[1].time).to.match(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ$/);
+      expect(record2.minutesGranted).to.equal(300);
+      expect(record2.updateID).to.exist;
+      expect(record2.accountID).to.equal(ACCOUNT_ID);
+    });
+
+    it("fails if the week isn't set up", function() {
+      return funcs.addTime(ACCOUNT_ID, WEEK1, -5, 'Not listening').then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('week-missing');
+        }
+      );
+    });
+
+    it("fails if there isn't enough time", async function() {
+      await funcs.setWeek(ACCOUNT_ID, WEEK1, 300);
+
+      await funcs.addTime(ACCOUNT_ID, WEEK1, -301, 'Not listening').then(
+        () => expect.fail(null, null, 'Expected error'),
+        err => {
+          expect(err.code).to.equal('insufficient-time');
+        }
+      );
+    });
+  });
+});
---
+++ server/funcs.ts
@@ -0,0 +1,321 @@
+import * as aws from 'aws-sdk';
+import { AWSError } from 'aws-sdk';
+import * as d3timeFormat from 'd3-time-format';
+import { reject } from 'async';
+import uuidv1 = require('uuid/v1');
+
+class ServerError extends Error {
+  code: string;
+
+  constructor(message: string, code: string) {
+    super(message);
+    Error.captureStackTrace(this, ServerError);
+    this.code = code;
+  }
+}
+
+const isoWeekParser = d3timeFormat.timeParse('%Y-W%V');
+
+const db = new aws.DynamoDB({apiVersion: '2012-08-10', endpoint: 'http://localhost:8000', region: 'us-west-2'});
+const docClient = new aws.DynamoDB.DocumentClient({apiVersion: '2012-08-10', endpoint: 'http://localhost:8000', region: 'us-west-2'});
+
+const WEEKS_TABLE = 'Weeks';
+const ACCOUNTS_TABLE = 'Accounts';
+
+export interface AccountRecord {
+  accountID: string;
+  recentPositiveReasons: string[];
+  recentNegativeReasons: string[];
+}
+
+export interface TimeRecord {
+  /** Number of minutes added (or removed). Cannot be zero. */
+  minutesAdded: number;
+
+  /** The reason for the addition or removal. Cannot be empty. */
+  reason: string;
+
+  /** The date the infraction took place. */
+  time: string;
+}
+
+export interface WeekRecord {
+  accountID: string;
+
+  /** ISO 8601 week (e.g. "2018-W03"). */
+  weekID: string;
+
+  /** Number of minutes initially granted. */
+  minutesGranted: number;
+
+  /** Changes made for this week. */
+  changes: TimeRecord[];
+
+  /** UUID for this update for optimistic concurrency. */
+  updateID: string;
+}
+
+export function createAccountsTable() {
+  return db.createTable({
+    TableName: ACCOUNTS_TABLE,
+    KeySchema: [
+      { AttributeName: 'accountID', KeyType: 'HASH' },
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'accountID', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: { ReadCapacityUnits: 20, WriteCapacityUnits: 5 }
+  }).promise();
+}
+
+export function createWeeksTable() {
+  return db.createTable({
+    TableName: WEEKS_TABLE,
+    KeySchema: [
+      { AttributeName: 'accountID', KeyType: 'HASH' },
+      { AttributeName: 'weekID', KeyType: 'RANGE' },
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'accountID', AttributeType: 'S' },
+      { AttributeName: 'weekID', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: { ReadCapacityUnits: 20, WriteCapacityUnits: 5 }
+  }).promise();
+}
+
+export function deleteWeeksTable() {
+  return db.deleteTable({ TableName: WEEKS_TABLE }).promise();
+}
+
+export function deleteAccountsTable() {
+  return db.deleteTable({ TableName: ACCOUNTS_TABLE }).promise();
+}
+
+export function getRecentReasons(accountID: string, positive: boolean, from: number, size: number): Promise<string[]> {
+  if(from < 0 || size <= 0)
+    return Promise.reject(new ServerError('From or size parameters invalid.', 'params-invalid'));
+
+  const REASONS_ATTRIBUTE = positive ? 'recentPositiveReasons' : 'recentNegativeReasons';
+
+  return docClient.get({
+    TableName: ACCOUNTS_TABLE,
+    Key: { 'accountID': accountID },
+    ProjectionExpression: REASONS_ATTRIBUTE,
+  }).promise().then(
+    response => {
+      if(!response.Item)
+        return Promise.reject(new ServerError('Account not found.', 'account-not-found'));
+
+      const reasons: string[] = response.Item[REASONS_ATTRIBUTE];
+      const counts = reasons.reduce((map, reason) => {
+        map.set(reason, (map.get(reason) || 0) + 1);
+        return map;
+      }, new Map<string, number>());
+
+      const result = Array.from(counts.entries()).sort((a, b) => (a[1] > b[1]) ? -1 : ((a[1] < b[1]) ? 1 : 0)).slice(from, from + size).map(r => r[0]);
+      return Promise.resolve(result);
+    },
+    err => {
+      if(err.code === 'ResourceNotFoundException')
+        return Promise.reject(new ServerError('Account not found.', 'account-not-found'));
+
+      return Promise.reject(err);
+    }
+  );
+}
+
+export function createAccount(accountID: string): Promise<void> {
+  const account: AccountRecord = {
+    accountID: accountID,
+    recentPositiveReasons: [],
+    recentNegativeReasons: [],
+  };
+
+  return docClient.put({
+    TableName: ACCOUNTS_TABLE,
+    Item: account,
+    ConditionExpression: 'attribute_not_exists(accountID)',
+  }).promise().then(
+    response => Promise.resolve(),
+    err => {
+      if(err.code === 'ConditionalCheckFailedException')
+        return Promise.reject(new ServerError('Account already exists.', 'account-exists'));
+
+      return Promise.reject(err);
+    }
+  );
+}
+
+/**
+ * Determines whether the given account name exists.
+ * accountID: Account to check.
+ */
+async function checkAccount(accountID: string): Promise<boolean> {
+  const response = await docClient.get({ TableName: ACCOUNTS_TABLE, Key: { accountID: accountID }, ProjectionExpression: 'accountID' }).promise();
+  return !!response.Item;
+}
+
+export function addTime(accountID: string, weekID: string, minutes: number, reason: string): Promise<WeekRecord> {
+  // Ensure week is valid
+  if(!isoWeekParser(weekID))
+    return Promise.reject(new ServerError('Invalid week specifier.', 'invalid-week'));
+
+  minutes = Math.floor(minutes);
+  reason = reason.trim();
+
+  if(minutes === 0)
+    return Promise.reject(new ServerError('Minutes cannot be zero.', 'missing-minutes'));
+
+  if(reason === '')
+    return Promise.reject(new ServerError('Reason cannot be empty.', 'missing-reason'));
+
+  return docClient.get({
+    TableName: WEEKS_TABLE,
+    Key: {
+      accountID: accountID,
+      weekID: weekID,
+    }
+  }).promise().then(
+    response => {
+      if(!response.Item)
+        return Promise.reject(new ServerError('Week has not been set up yet.', 'week-missing'));
+      
+      const week = response.Item as WeekRecord;
+
+      // Ensure we don't go negative
+      const minutesLeft = week.changes.reduce<number>((count, record) => count + record.minutesAdded, week.minutesGranted);
+
+      if(minutesLeft + minutes < 0)
+        return Promise.reject(new ServerError(`Cannot take away ${-minutes} minutes with only ${minutesLeft} minutes remaining.`, 'insufficient-time'));
+
+      // Checks done, make the change happen
+      week.changes.push({ reason: reason, minutesAdded: minutes, time: d3timeFormat.isoFormat(new Date()) });
+
+      return docClient.update({
+        TableName: WEEKS_TABLE,
+        Key: {
+          accountID: accountID,
+          weekID: weekID
+        },
+        UpdateExpression: 'set changes = list_append(changes, :c)',
+        ExpressionAttributeValues: {
+          ':c': [{ reason: reason, minutesAdded: minutes, time: d3timeFormat.isoFormat(new Date()) }]
+        },
+      }).promise().then(updateResponse => {
+        // Update the recently used reason list
+        return addReason(accountID, minutes > 0, reason).then(a => week);
+      });
+    }
+  );
+}
+
+function addReason(accountID: string, positive: boolean, reason: string): Promise<void> {
+  const REASONS_ATTRIBUTE = positive ? 'recentPositiveReasons' : 'recentNegativeReasons';
+  const REASONS_TO_KEEP = 100;
+
+  return docClient.get({
+    TableName: ACCOUNTS_TABLE,
+    Key: { 'accountID': accountID },
+    ProjectionExpression: REASONS_ATTRIBUTE,
+  }).promise().then(
+    response => {
+      if(!response.Item)
+        return Promise.reject(new ServerError('Account not found.', 'missing-account'));
+
+      // Amend the list and remove old entries
+      const reasons: string[] = response.Item[REASONS_ATTRIBUTE];
+      reasons.push(reason);
+
+      if(reasons.length > REASONS_TO_KEEP)
+        reasons.shift();
+
+      return docClient.update({
+        TableName: ACCOUNTS_TABLE,
+        Key: {
+          accountID: accountID,
+        },
+        UpdateExpression: 'set #reasons = :c',
+        ExpressionAttributeNames: { '#reasons': REASONS_ATTRIBUTE },
+        ExpressionAttributeValues: { ':c': reasons }
+      }).promise().then(() => Promise.resolve());
+    }
+  );
+}
+
+export async function setWeek(accountID: string, weekID: string, minutesGranted: number): Promise<WeekRecord> {
+  minutesGranted = Math.floor(minutesGranted);
+
+  if(minutesGranted < 0)
+    throw new ServerError(`Can't grant less than zero minutes.`, 'insufficient-time');
+
+  // Ensure week is valid
+  if(!isoWeekParser(weekID))
+    throw new ServerError('Invalid week specifier.', 'invalid-week');
+
+  if(!(await checkAccount(accountID)))
+    throw new ServerError('Invalid account.', 'invalid-account');
+
+  let week: WeekRecord = {
+    accountID: accountID,
+    weekID: weekID,
+    minutesGranted: minutesGranted,
+    changes: [],
+    updateID: uuidv1(),
+  };
+
+  try {
+    await docClient.put({
+      TableName: WEEKS_TABLE,
+      Item: week,
+      ConditionExpression: 'attribute_not_exists(weekID)',
+    }).promise();
+
+    return week;
+  }
+  catch(errObj) {
+    const err = errObj as AWSError;
+
+    if(err.code !== 'ConditionalCheckFailedException')
+      throw err;
+
+    // The week exists already, so verify we won't set an invalid result
+    const response = await docClient.get({ TableName: WEEKS_TABLE, Key: { accountID: accountID, weekID: weekID }, ConsistentRead: true }).promise();
+    week = response.Item as WeekRecord;
+
+    // Ensure we don't go negative
+    const minutesLeft = week.changes.reduce((count, record) => count + record.minutesAdded, minutesGranted);
+
+    if(minutesLeft < 0)
+      throw new ServerError(`Granting that much time for the week would put the total time for the week less than zero.`, 'insufficient-time');
+
+    const oldUpdateID = week.updateID;
+
+    week.minutesGranted = minutesGranted;
+    week.updateID = uuidv1();
+
+    await docClient.put({
+      TableName: WEEKS_TABLE,
+      Item: week,
+      ConditionExpression: 'updateID = :uuid',
+      ExpressionAttributeValues: {
+        ':uuid': oldUpdateID,
+      }
+    }).promise();
+
+    return week;
+  }
+}
+
+export async function getWeek(accountID: string, weekID: string): Promise<WeekRecord> {
+  // Ensure week is valid
+  if(!isoWeekParser(weekID))
+    throw new ServerError('Invalid week specifier.', 'invalid-week');
+
+  const response = await docClient.get({
+    TableName: WEEKS_TABLE,
+    Key: { accountID: accountID, weekID: weekID },
+  }).promise();
+
+  return response.Item as WeekRecord;
+}
+
